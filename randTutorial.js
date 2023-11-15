d7f60365a7cd3e4a0629423dddd7f2b352a3a524
Websitehosterio <124309800+Websitehosterio@users.noreply.github.com>
2023-11-15 11:21:47 -0500

    Delete randTutorial.js

--- randTutorial.js
+++
@@ -1,6 +0,0 @@
-var randNum = Math.floor(Math.random() * 2);
-if(randNum == 0){
-  document.getElementById("examples").innerHTML = "<div id='examples'><iframe src='example1.html'></iframe></div>";
-}else{
-  document.getElementById("examples").innerHTML = "<div id='examples'><iframe src='example2.html'></iframe></div>";
-}
